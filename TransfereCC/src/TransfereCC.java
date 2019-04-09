8528057c36791c3cbeb6d93653340e7a9d9f5c2b
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2019-04-10 00:32:06 +0100

    perfeito

--- TransfereCC/src/TransfereCC.java
+++ TransfereCC/src/TransfereCC.java
@@ -1,25 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import agenteUDP.*;
 
-public class TransfereCC {
+public class TransfereCC implements Runnable {
 
+    public TransfereCC() {
 
-    public static void main(String[] args) {
+    }
+
+    public void run() {
+
         BufferedReader in
                 = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("PUT (1) ou GET (2) ?");
+
+        System.out.println("Modo servidor ou cliente?");
         try {
             String escolha = in.readLine();
+
+            switch (escolha) {
+                case "Cliente":
+                    System.out.println("Qual é o servidor que deseja criar conexão?");
+                    String servidor = in.readLine();
+                    AgenteUDP.inicializarConexao(servidor);
+                case "Servidor":
+                    AgenteUDP.aceitaConexao();
+                    break;
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        /*switch (escolha) {
-            case 1:
-
-        }*/
+    public static void main (String[] args) {
+        (new Thread(new TransfereCC())).run();
     }
 }
--- TransfereCC/src/UDPServer.java
+++ TransfereCC/src/UDPServer.java
@@ -1,5 +1,3 @@
-package agenteUDP;
-
 import java.io.*;
 import java.net.*;
 
