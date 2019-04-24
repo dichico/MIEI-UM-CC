c2aec60e2b269624c35989c9bfaf87d959ad9926
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2019-04-24 12:02:27 +0100

    Update Servidor.java

--- TransfereCC/src/Servidor.java
+++ TransfereCC/src/Servidor.java
@@ -6,7 +6,6 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  * Source-Code para a Classe Servidor
